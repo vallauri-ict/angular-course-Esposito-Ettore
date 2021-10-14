c73e8b94267eb4961740340f4b86b945489f6edd
Esposito-Ettore <e.esposito.1520@vallauri.edu>
2021-10-14 11:35:45 +0200

    ngFor & @Input statements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = '5^B INF';
+
+  studentList = [
+    {name : "Ettore Esposito", hobby : 'karate', sex : 'M', isPro : false},
+    {name : "Bianca Teleman", hobby : 'calcio', sex : 'F', isPro : false},
+    {name : "Paolo Giordano", hobby : 'mostrare la patente', sex : 'M', isPro : false},
+    {name : "Giada Valinotto", hobby : 'karate', sex : 'F', isPro : false},
+    {name : "Gesù Cristo", hobby : 'karate', sex : 'M', isPro : false},
+    {name : "Federico Mameli", hobby : 'calcio', sex : 'M', isPro : false},
+    {name : "Martina Velardi", hobby : 'karate', sex : 'F', isPro : false},
+    {name : "Marta Baudracco", hobby : 'karate', sex : 'F', isPro : false},
+  ]
 }
--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-student',
@@ -6,34 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./student.component.css']
 })
 export class StudentComponent implements OnInit {
-
-  studentList = [
-    {name : "Ettore Esposito", hobby : 'karate', sex : 'M', isPro : false},
-    {name : "Bianca Teleman", hobby : 'calcio', sex : 'F', isPro : false},
-    {name : "Paolo Giordano", hobby : 'mostrare la patente', sex : 'M', isPro : true},
-    {name : "Giada Valinotto", hobby : 'karate', sex : 'F', isPro : false},
-    {name : "Gesù Cristo", hobby : 'karate', sex : 'M', isPro : true},
-    {name : "Federico Mameli", hobby : 'calcio', sex : 'M', isPro : true},
-    {name : "Martina Velardi", hobby : 'karate', sex : 'F', isPro : true},
-    {name : "Marta Baudracco", hobby : 'karate', sex : 'F', isPro : false},
-  ]
-
-  student :any;
+  @Input() student :any;
 
   constructor() {
-    this.student = this.RandomStudent();
+    
   }
 
   ngOnInit(): void {
+    this.student.isPro = this.RandomStudentPro();
   }
 
   onStudentClick () {
-    this.student = this.RandomStudent();
+    this.student.isPro = !this.student.isPro;
   }
 
-  private RandomStudent() {
-    let num = Math.floor(Math.random() * this.studentList.length);
-    return this.studentList[num];
+  private RandomStudentPro() {
+    let num = Math.floor(Math.random() * 2);
+    return num == 1 ? true : false;
   }
-
 }
