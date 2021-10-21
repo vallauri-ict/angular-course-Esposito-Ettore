3c0ebf8232840cb4545c203397f4f07f104476e6
Esposito-Ettore <e.esposito.1520@vallauri.edu>
2021-10-21 11:34:56 +0200

    add student

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,9 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = '5^B INF';
+  @ViewChild("txtName") txtName !:ElementRef;
 
-  studentList = [
+  title = '5^B INF';
+  hobbies = ["calcio", "karate", "paddle"];
+
+  studentName :string = "";
+  studentSex :string = "M";
+  studentHobby :string = this.hobbies[0];
+  studentList :any[] = [];
+  studentRepository = [
     {name : "Ettore Esposito", hobby : 'karate', sex : 'M', isPro : false},
     {name : "Bianca Teleman", hobby : 'calcio', sex : 'F', isPro : false},
     {name : "Paolo Giordano", hobby : 'mostrare la patente', sex : 'M', isPro : false},
@@ -17,5 +24,22 @@ export class AppComponent {
     {name : "Federico Mameli", hobby : 'calcio', sex : 'M', isPro : false},
     {name : "Martina Velardi", hobby : 'karate', sex : 'F', isPro : false},
     {name : "Marta Baudracco", hobby : 'karate', sex : 'F', isPro : false},
-  ]
+  ];
+
+  constructor() {
+    for (let i = 0; i < 4; i++)
+    {
+      let rnd :number = Math.floor(Math.random() * this.studentRepository.length);
+      this.studentList.push(this.studentRepository[rnd]);
+      this.studentRepository.splice(rnd, 1);
+    }
+  }
+
+  onCreateStudent() {
+    let newStudent = { name : this.studentName, hobby : this.studentHobby , sex : this.studentSex, isPro : false };
+    this.studentList.push(newStudent);
+    this.studentName = "";
+
+    this.txtName.nativeElement.focus();
+  }
 }
